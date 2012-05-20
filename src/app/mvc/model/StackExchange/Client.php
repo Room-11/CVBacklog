0afadc68c80410792a52fa6c2bc21007d0b47479
gooh <info@gordon-oheim.de>
2012-05-20 11:18:41 +0200

    added logging of SE.API reponse when no items are found

--- src/app/mvc/model/StackExchange/Client.php
+++ src/app/mvc/model/StackExchange/Client.php
@@ -25,6 +25,8 @@ class Client
             $result = $this->executeRequest($this->formatEndpoint($batch));
             if (isset($result->items)) {
                 $data = array_merge($data, $result->items);
+            } else {
+                error_log("Error fetching batch $i: " . var_export($result, true));
             }
         }
         return $data;
